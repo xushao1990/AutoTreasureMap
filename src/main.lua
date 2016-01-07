5561378b6908809564797ecd1efcd0ea5d9cb24a
Tongtong Xu <xutongtong@breadtrip.com>
2016-01-07 15:28:13 +0800

    change

--- src/main.lua
+++ src/main.lua
@@ -1,11 +1,10 @@
 function main()
-	x,y = getWaBaoPosition()
-	click(x,y)
-end
-
-function getWaBaoPosition()
-	x,y = findMultiColorInRegionFuzzy(0x540207,"38|0|0xdf9f4c,10|31|0xd18730,53|33|0x7b886b",100,0,0,1242,2208)
-	return x,y
+	x,y = findMultiColorInRegionFuzzy(0xfbf5bf,"-41|-6|0xfefdd5,-40|9|0xf6e19b,-20|15|0xd6b975",100,0,0,2207,1241);
+	if x~= -1 and y ~= -1 then
+		click(x,y)
+	else
+		dialog("未找到合适的坐标",0);
+	end
 end
 
 function click(x,y)
